e900dbf44fd49ebc3f7c1c1e6f9884b4101985c1
limprog <ilinivan1602@gmail.com>
2025-05-14 23:06:34 +0300

    unification triggerHandler and unconditionalHandler; add return type to some function

--- src/middleware/ChancedEventsHandlers.py
+++ src/middleware/ChancedEventsHandlers.py
@@ -9,7 +9,7 @@ from src.middleware.UserHandlers import bot_logger
 
 # TODO: политика проверки триггеров (any | all)
 
-def send_by_chance(bot: telebot.TeleBot, message: telebot.types.Message, name: str, **kwargs):
+def send_by_chance(bot: telebot.TeleBot, message: telebot.types.Message, name: str, **kwargs) -> bool:
     chance, text, reply = messages.format_chanced(name, **kwargs)
     if random.uniform(0, 1) <= chance:
         if reply:
@@ -36,27 +36,22 @@ def check_triggers(name: str, message: telebot.types.Message):
     return all(triggered) if triggered else False
 
 
-def send_by_trigger(bot: telebot.TeleBot, message: telebot.types.Message, name: str, **kwargs):
+def send_by_trigger(bot: telebot.TeleBot, message: telebot.types.Message, name: str, **kwargs) -> bool:
     if check_triggers(name, message):
         send_by_chance(bot, message, name, **kwargs)
         return True
     return False
 
 
-def unconditionalHandler(bot: telebot.TeleBot, message: telebot.types.Message):
-    for message_name in messages.chancedNames():
-        if not messages.is_conditional(message_name):
-            sent = send_by_chance(bot, message, message_name)
-            if sent:
-                break
-
-
-def triggeredHandler(bot: telebot.TeleBot, update: telebot.types.Update):
-    if not update.message or not update.message.text:
+def message_chanse_trigger_hanlder(bot: telebot.TeleBot, message: telebot.types.Message) -> None:
+    if not message.text:
         return
 
     for message_name in messages.chancedNames():
-        if messages.is_conditional(message_name):
-            sent = send_by_trigger(bot, update.message, message_name)
-            if sent:
-                break
+        if messages.is_conditional(message_name) == False:
+            sent = send_by_chance(bot, message, message_name)
+        else:
+            sent = send_by_trigger(bot, message, message_name)
+
+        if sent:
+            return
